0707d9a2b8bd7350fb8ad4ec0d9a483ec428f261
Manvi Kapoor <kapoor.manvi11@gmail.com>
2020-07-10 01:10:09 +0000

    added permission features and related error messages

--- express_server.js
+++ express_server.js
@@ -8,8 +8,8 @@ app.set("view engine", "ejs");
 app.use(cookieParser());
 
 const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  b6UTxQ: { longURL: "https://www.tsn.ca", userID: "aJ48lW" },
+  i3BoGr: { longURL: "https://www.google.ca", userID: "aJ48lP" }
 };
 
 const users = { 
@@ -53,32 +53,55 @@ app.get("/urls", (req, res) => {
   console.log("req.cookies: ", req.cookies);
   const userId = req.cookies.user_id;
   const user = users[userId];
-  let templateVars = { urls: urlDatabase, 
-    user: user,
-  };
-  res.render("urls_index", templateVars);
+  let urlsForLoggedInUser = {};
+  if (userId) {
+
+    for (obj in urlDatabase) {
+      console.log(urlDatabase[obj]);
+      if (urlDatabase[obj].userID === userId) {
+        urlsForLoggedInUser[obj] = urlDatabase[obj];
+      }
+    }
+    let templateVars = { urls: urlsForLoggedInUser,
+      user: user,
+    };
+    res.render("urls_index", templateVars);
+  } else {
+    res.status(403).send('<h1>Halt! Who goes there??</h1> <p>Shoot, it looks like you forgot to login. Back up, and try again.</p>');
+  }
 });
 
-//A GET route to show the form that allows user to enter a URL to be shortened
+//Displays the form that allows user to enter a URL to be shortened
 app.get("/urls/new", (req, res) => {
   const userId = req.cookies.user_id;
   const user = users[userId];
   let templateVars = {
     user: user,
   };
-  
-  res.render("urls_new", templateVars);
+  if (user) {
+    res.render("urls_new", templateVars);
+
+  } else {
+    res.redirect("/login");
+  }
 });
 
+//Displays the Edit URL page for a given URL
 app.get("/urls/:shortURL", (req, res) => {
   const userId = req.cookies.user_id;
   const user = users[userId];
-  let templateVars = { 
-    shortURL: req.params.shortURL, 
-    longURL: urlDatabase[req.params.shortURL], 
-    user: user,
-  };
-  res.render("urls_show", templateVars);
+
+  if (userId === urlDatabase[req.params.shortURL].userID) {
+    let templateVars = { 
+      shortURL: req.params.shortURL, 
+      longURL: urlDatabase[req.params.shortURL].longURL, 
+      user: user,
+    };
+    res.render("urls_show", templateVars);
+    
+  } else {
+    res.status(403).send('<h1>Uh, this URL does not belong to you. </h1> <p>Sharing is usually caring, but not today. Stick to your own URLs please! </p>');
+  }
 });
 
 //A POST request to accept the user input from the URL shortener form
@@ -86,27 +109,43 @@ app.post("/urls", (req, res) => {
   console.log(req.body);  // Log the POST request body to the console
   const shortURL = generateRandomString();
   const longURL = req.body.longURL;
-  urlDatabase[shortURL] = longURL; // shortURL-longURL key-value pair are saved to the urlDatabase when it receives a POST request to /urls
-  res.redirect(`/urls/${shortURL}`); // generates short url random string
+  const userId = req.cookies.user_id;
+  urlDatabase[shortURL] = {longURL: longURL, userID: userId }; // shortURL-longURL key-value pair are saved to the urlDatabase when it receives a POST request to /urls
+  res.redirect('/urls'); // generates short url random string
   res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
 
 //Takes user to the website as indicated by longURL when shortUL is clicked
 app.get("/u/:shortURL", (req, res) => {
-  const longURL = urlDatabase[req.params.shortURL];
+  const longURL = urlDatabase[req.params.shortURL].longURL;
   res.redirect(longURL);
 });
 
 //Delete URL button added to main page that lists all shortened URLs - deletes URL from urlDatabase
 app.post("/urls/:shortURL/delete", (req, res) => {
-  delete urlDatabase[req.params.shortURL];
-  res.redirect("/urls");
+  const userId = req.cookies && req.cookies.user_id
+
+  if (userId === urlDatabase[req.params.shortURL].userID) {
+    delete urlDatabase[req.params.shortURL];
+    res.redirect("/urls");
+  } else {
+    res.status(403).send('<h1>Uh, this URL does not belong to you. </h1> <p>Sharing is usually caring, but not today. Stick to deleting your own URLs please! </p>');
+  }
+  
 });
 
 //Edit URLs feature - edit button on main page takes you to a new page to enter a new long URL to replace an existing shortened URL
 app.post("/urls/:shortURL", (req, res) => {
-  urlDatabase[req.params.shortURL] = req.body.longURL;
-  res.redirect("/urls");
+  const userId = req.cookies && req.cookies.user_id
+  
+  if (userId === urlDatabase[req.params.shortURL].userID) {
+    urlDatabase[req.params.shortURL].longURL = req.body.longURL;
+    res.redirect("/urls");
+  
+  } else {
+    res.status(403).send('<h1>Uh, this URL does not belong to you. </h1> <p>Sharing is usually caring, but not today. Stick to editing your own URLs please! </p>');
+  }
+  
 });
 
 // Accepts email and password input on the login page, compares them to stored values for a match and places cookie if there is a match
@@ -120,13 +159,7 @@ app.post("/login", (req, res) => {
     
     const userId = getIdByEmail(email);
     res.cookie('user_id', userId); 
-    
-
-
     res.redirect("/urls");
-
-
-
   } else {
     res.status(403).send('<h1>403 Error</h1> <p>Uh, did you forget something...? The password and email do not match.</p>');
   }
@@ -147,7 +180,7 @@ app.get("/login", (req, res) => {
 app.post("/logout", (req, res) => {
   
   res.clearCookie('user_id', "");
-  res.redirect("/urls");
+  res.redirect("/login");
 });
 
 // Displays Registration Page
